f9079ba612d53f472a4f6b92bcbb0d94331b7cca
Lewis Taylor <lewis@dangerfarms.com>
2016-05-12 11:43:31 +0100

    Unset environmental variables to try to minimise security holes

--- plugin/main.py
+++ plugin/main.py
@@ -13,24 +13,30 @@ def main():
     payload = drone.plugin.get_input()
     vargs = payload["vargs"]
 
-    # Required fields should raise an error
-    os.environ["RANCHER_URL"] = vargs['url']
-    os.environ["RANCHER_ACCESS_KEY"] = vargs['access_key']
-    os.environ["RANCHER_SECRET_KEY"] = vargs['secret_key']
+    # Change directory to deploy path
+    deploy_path = payload["workspace"]["path"]
+    os.chdir(deploy_path)
 
     # Optional fields
     compose_file = vargs.get('compose_file', 'docker-compose.yml')
     stack = vargs.get('stack', payload['repo']['name'])
     services = vargs.get('services', '')
 
-    # Change directory
-    deploy_path = payload["workspace"]["path"]
-    os.chdir(deploy_path)
+    # Set Required fields for rancher-compose to work
+    # Should raise an error if they are not declared
+    os.environ["RANCHER_URL"] = vargs['url']
+    os.environ["RANCHER_ACCESS_KEY"] = vargs['access_key']
+    os.environ["RANCHER_SECRET_KEY"] = vargs['secret_key']
 
     rc_args = [
         "rancher-compose", "-f", compose_file, "-p", stack, "up", services,
     ]
     subprocess.call(rc_args)
+
+    # Unset environmental variables, no point in them hanging about
+    del os.environ['RANCHER_URL']
+    del os.environ['RANCHER_ACCESS_KEY']
+    del os.environ['RANCHER_SECRET_KEY']
 
 
 if __name__ == "__main__":
